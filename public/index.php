4f3b3c0df7b5f1d43164a9d36eb0abf88568d9ad
yudi-art <yudi@sman1baregbeg.sch.id>
2026-08-16 21:58:13 +0700

    index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,17 @@
 <?php
-header('Content-Type: application/json');
-http_response_code(200);
-echo json_encode([
-    'status' => 'Absensi SMAN1 Baregbeg API Online',
-    'version' => '1.0',
-    'mode' => 'anti-crash ready',
-    'time' => date('Y-m-d H:i:s')
-]);
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
