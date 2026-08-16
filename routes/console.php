1943cf19ddec04576062467a9b9f0610199b2f2b
yudi-art <yudi@sman1baregbeg.sch.id>
2026-08-16 22:53:46 +0700

    console.php

--- routes/console.php
+++ routes/console.php
@@ -1,3 +1,8 @@
 <?php
+
 use Illuminate\Foundation\Inspiring;
-\Illuminate\Support\Facades\Artisan::command('inspire', function () { \$this->comment(Inspiring::quote()); })->purpose('Display an inspiring quote');
+use Illuminate\Support\Facades\Artisan;
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
