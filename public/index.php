57b63b85f9379a06cc39221f3531d294ca3340b7
yudi-art <yudi@sman1baregbeg.sch.id>
2026-08-16 21:44:22 +0700

    index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-define('LARAVEL_START', microtime(true));
-require __DIR__.'/../vendor/autoload.php';
-$app = require_once __DIR__.'/../bootstrap/app.php';
-$app->handleRequest(Request::capture());
+header('Content-Type: application/json');
+http_response_code(200);
+echo json_encode([
+    'status' => 'Absensi SMAN1 Baregbeg API Online',
+    'version' => '1.0',
+    'mode' => 'anti-crash ready',
+    'time' => date('Y-m-d H:i:s')
+]);
